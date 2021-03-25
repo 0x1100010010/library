604d68d1f91a7db7778cd9e3adb7ef9fa906a6e4
bashforger <bash.forger@gmail.com>
2021-03-25 16:59:04 +0500

    Rendered dummy data

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
+'use strict';
 
-const mockBooks = [
+const myLibrary = [
   {
     title: 'Book-1', 
     author: 'Author-1',
@@ -19,3 +20,32 @@ const mockBooks = [
     status: true
   }
 ]
+
+function Book(title, author, pages, status) {
+  title =  title
+  author = author
+  pages = pages
+  status = status
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function loadLibrary () {
+  let entries = "";
+  myLibrary.forEach( 
+    record => { 
+      
+      entries += `<tr><td class="px-6 py-4 whitespace-nowrap"><div class="text-sm text-gray-500">${record.title}</div></td>`
+      entries += `<td class="px-6 py-4 whitespace-nowrap"><div class="text-sm text-gray-500">${record.author}</div></td>`
+      entries += `<td class="px-6 py-4 whitespace-nowrap"><div class="text-sm text-gray-500">${record.pages}</div></td>`
+      entries += `<td class="px-6 py-4 whitespace-nowrap text-center"><span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">${record.status}</span></td>`
+      entries += `<td class="px-6 py-4 whitespace-nowrap text-center"><a href="#" class="text-indigo-600 hover:text-indigo-900">Drop</a></td></tr>`
+      
+    }
+  );
+  document.getElementById('library-entries').innerHTML = entries;
+}
+
+loadLibrary();
