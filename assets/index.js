419340911ce91321a44dc1b47ec3e9bbcf26f7e2
bashforger <bash.forger@gmail.com>
2021-03-25 16:04:29 +0500

    Init mock data

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+
+const mockBooks = [
+  {
+    title: 'Book-1', 
+    author: 'Author-1',
+    pages: 300,
+    status: true
+  },
+  {
+    title: 'Book-2', 
+    author: 'Author-2',
+    pages: 400,
+    status: false
+  },
+  {
+    title: 'Book-3', 
+    author: 'Author-3',
+    pages: 500,
+    status: true
+  }
+]
